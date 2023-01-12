8f1cfb2438a18d63bc3c2ec87b3e8e74cbfa2f48
Corina Udrescu <corinutz@gmail.com>
2023-01-12 08:43:51 +0100

    Simple persons list and add form

--- src/contact-book.tsx
+++ src/contact-book.tsx
@@ -1,5 +1,67 @@
+import { useState } from "react";
+
+type Person = {
+  name: string;
+  city: string;
+};
+
 const ContactBook = () => {
-  return <div>Add your solution here</div>;
+  const [newPerson, setNewPerson] = useState<Person>(null);
+  const [persons, setPersons] = useState<Person[]>([]);
+
+  const handleAddContact = (e) => {
+    e.preventDefault();
+
+    if (!newPerson) {
+      return;
+    }
+
+    setPersons([...persons, { ...newPerson }]);
+    setNewPerson(null);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleAddContact}>
+        <div style={{ border: "1px solid lightgray", padding: "10px" }}>
+          <h3>Add a new contact</h3>
+
+          <label style={{ paddingRight: "10px" }}>
+            Name&nbsp;
+            <input
+              type="text"
+              value={newPerson?.name || ""}
+              onChange={(e) =>
+                setNewPerson({ ...newPerson, name: e.target.value })
+              }
+            />
+          </label>
+
+          <label style={{ paddingRight: "10px" }}>
+            City&nbsp;
+            <input
+              type="text"
+              value={newPerson?.city || ""}
+              onChange={(e) =>
+                setNewPerson({ ...newPerson, city: e.target.value })
+              }
+            />
+          </label>
+
+          <button type="submit">Add contact</button>
+        </div>
+      </form>
+      <div>
+        <ul>
+          {persons.map((person) => (
+            <li key={person.name}>
+              {person.name} ({person.city})
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default ContactBook;
